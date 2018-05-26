30c81a22902593d4afbf895df283338c9071e280
Kiir080 <brunocunha.1357@gmail.com>
2018-05-26 09:11:50 +0100

    foi adicionado o html/css da receção

---
+++ Bruno/JavaScript/Exercicios/Modulos/routes/rececaoRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+    mongoManager
+} = require('../Modulo_Mongoose/mongoManager');
+
+let mongoM = new mongoManager('hospital', 'doentes');
+
+const doenteSchema = require('../Modulo_Mongoose/schemas/doente');
+
+
+
+router.post('/addFicha',function(req,res){
+    const doente = mongoMan.connect(doenteSchema);
+    doente.find({
+        Numero_Utente : `${res.body.nUtente}`
+    }).exec((err,result)=>{
+        if (err) throw err;
+        if (result.length === 1) {
+            result.Ficha_Urgencia.push()
+        mongoMan.disconnect();
+        } else {
+       
+      }
+    })
+})
+
